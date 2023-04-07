7c95416cfb0add9915072fe38197d8231ff1d59b
Jozef Pažin <pazin@heypay.sk>
2023-04-07 15:23:05 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace GrayTransparencyTest
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        private Image imageDefault = new Image();
+        public Image ImageDefault
+        {
+            get { return imageDefault; }
+            private set { imageDefault = value; PropertyChangedInvoke(); }
+        }
+
+        private Image imageGray = new Image();
+        public Image ImageGray
+        {
+            get { return imageGray; }
+            private set { imageGray = value; PropertyChangedInvoke(); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void PropertyChangedInvoke([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+
+            InitializeImages();
+        }
+
+        private void InitializeImages()
+        {
+            string imageKey = "home";
+            Uri imageURI = new Uri($"pack://application:,,,/GrayTransparencyTest;component/Media/{imageKey}.png", UriKind.Absolute);
+            BitmapImage bitmapImage = new BitmapImage(imageURI);
+            ImageBrush opacityMask = new ImageBrush()
+            {
+                ImageSource = bitmapImage
+            };
+            ImageDefault = new Image()
+            {
+                Source = bitmapImage,
+                OpacityMask = opacityMask,
+            };
+            FormatConvertedBitmap bitmapGreyscale = new FormatConvertedBitmap(); bitmapGreyscale.BeginInit(); bitmapGreyscale.Source = new BitmapImage(imageURI); bitmapGreyscale.DestinationFormat = PixelFormats.Gray16; bitmapGreyscale.EndInit();
+            ImageGray = new Image()
+            {
+                Source = bitmapGreyscale,
+                OpacityMask = opacityMask,
+            };
+        }
+    }
+}
